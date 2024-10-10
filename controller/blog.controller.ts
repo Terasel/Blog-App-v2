e3c4682bbc4190b157017806779925f24ee912c1
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-10-10 13:49:26 +0200

    CRUD v3

--- controller/blog.controller.ts
+++ controller/blog.controller.ts
@@ -3,34 +3,7 @@ import { prisma } from "../infrastructure/dbconnection"
 
 const router = Router();
 
-router.get('/blog', async (req, res) => {
-    try {
-        const blogs = await prisma.blog.findMany({
-            where: {
-                deleted: false
-            }
-        })
-        if (!blogs) throw 'Empty'
-        res.send(blogs)
-    } catch (err) {
-        if (err = 'Empty') res.status(404).send('There are no blogs')
-    }
-})
-
-router.get('/blog/:id', async (req, res) => {
-    try {
-        const specificBlog = await prisma.blog.findFirst({
-            where: {
-                id: +req.params.id
-            }
-        })
-        if (!specificBlog) throw 'Empty'
-        res.status(200).send(specificBlog)
-    } catch (err) {
-        if (err = 'Empty') res.status(404).send('This blog does not exist')
-    }
-})
-
+// simpleUser
 router.post('/blog', async (req, res) => {
     try {
         const newBlog = await prisma.blog.create({
@@ -39,56 +12,7 @@ router.post('/blog', async (req, res) => {
         if (!newBlog) throw 'Not created'
         res.status(201).send(newBlog)
     } catch (err) {
-        if (err = 'Not created') res.status(404).send('This blog could not be created')
-    }
-})
-
-router.put('/blog/:id', async (req, res) => {
-    try {
-        const blogUpdate = await prisma.blog.update({
-            where: {
-                id: +req.params.id
-            },
-            data: req.body
-        })
-        if (!blogUpdate) throw 'Empty'
-        res.status(200).send(blogUpdate)
-    } catch (err) {
-        if (err = 'Empty') res.status(404).send('This blog does not exist')
-    }
-})
-
-router.delete('/blog/:id', async (req, res) => {
-    try {
-        const blogDelete = await prisma.blog.update({
-            where: {
-                id: +req.params.id,
-            },
-            data: {
-                deleted: true,
-            }
-        })
-        if (!blogDelete) throw 'Empty'
-        res.status(204).send(blogDelete)
-    } catch (err) {
-        if (err = 'Empty') res.status(404).send('This blog does not exist')
-    }
-})
-
-router.patch('/blog/:id/recover', async (req, res) => {
-    try {
-        const blogRecover = await prisma.blog.update({
-            where: {
-                id: +req.params.id
-            },
-            data: {
-                deleted: false
-            }
-        })
-        if (!blogRecover) throw 'Empty'
-        res.status(200).send(blogRecover)
-    } catch (err) {
-        if (err = 'Empty') res.status(404).send('This blog does not exist')
+        if (err = 'Not created') res.status(422).send('This blog could not be created')
     }
 })
 
@@ -126,4 +50,96 @@ router.patch('/blog/:id/disliked', async (req, res) => {
     }
 })
 
+router.get('/blog', async (req, res) => {
+    try {
+        const blogs = await prisma.blog.findMany({
+            where: {
+                deleted: false
+            }
+        })
+        if (!blogs) throw 'Empty'
+        res.status(200).send(blogs)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('There are no blogs')
+    }
+})
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const specificBlog = await prisma.blog.findFirst({
+            where: {
+                id: +req.params.id
+            }
+        })
+        if (!specificBlog) throw 'Empty'
+        res.status(200).send(specificBlog)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This blog does not exist')
+    }
+})
+
+router.get('/blog/:authorId/byauthor', async (req, res) => {
+    try {
+        const authorBlogs = await prisma.blog.findMany({
+            where: {
+                authorId: +req.params.authorId
+            }
+        })
+        if (!authorBlogs) throw 'Empty'
+        res.status(200).send(authorBlogs)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This blog does not exist')
+    }
+})
+
+router.delete('/blog/:id', async (req, res) => {
+    try {
+        const blogDelete = await prisma.blog.update({
+            where: {
+                id: +req.params.id,
+            },
+            data: {
+                deleted: true,
+            }
+        })
+        if (!blogDelete) throw 'Empty'
+        res.status(204).send(blogDelete)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This blog does not exist')
+    }
+})
+
+router.put('/blog/:id', async (req, res) => {
+    try {
+        const blogUpdate = await prisma.blog.update({
+            where: {
+                id: +req.params.id
+            },
+            data: req.body
+        })
+        if (!blogUpdate) throw 'Empty'
+        res.status(200).send(blogUpdate)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This blog does not exist')
+    }
+})
+
+router.patch('/blog/:id/recover', async (req, res) => {
+    try {
+        const blogRecover = await prisma.blog.update({
+            where: {
+                id: +req.params.id
+            },
+            data: {
+                deleted: false
+            }
+        })
+        if (!blogRecover) throw 'Empty'
+        res.status(200).send(blogRecover)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This blog does not exist')
+    }
+})
+
+// admin
 export default router
--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -3,16 +3,82 @@ import { prisma } from "../infrastructure/dbconnection"
 
 const router = Router();
 
-router.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-});
-
+// simpleUser
 router.post('/users', async (req, res) => {
-    const newUser = await prisma.user.create({
-        data: req.body,
-    });
-    res.json(newUser)
-});
+    try {
+        const newUser = await prisma.user.create({
+            data: req.body,
+        })
+        if (!newUser) throw 'Not created'
+        res.status(201).send(newUser)
+    } catch (err) {
+        if (err = 'Not created') res.status(422).send('This user could not be created')
+    }
+})
+
+router.put('/users/:id', async (req, res) => {
+    try {
+        const userUpdate = await prisma.user.update({
+            where: {
+                id: +req.params.id
+            },
+            data: req.body
+        })
+        if (!userUpdate) throw 'Empty'
+        res.status(200).send(userUpdate)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This user does not exist')
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany({
+            where: {
+                banned: false
+            }
+        })
+        if (!users) throw 'No users'
+        res.status(200).send(users);
+    } catch (err) {
+        if (err = 'No users') res.status(404).send('There are no users')
+    }
+})
+
+//admin
+
+router.patch('/users/:id/ban', async (req, res) => {
+    try {
+        const userBan = await prisma.user.update({
+            where: {
+                id: +req.params.id,
+            },
+            data: {
+                banned: true,
+            }
+        })
+        if (!userBan) throw 'Empty'
+        res.status(204).send(userBan)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This user does not exist')
+    }
+})
+
+router.patch('/users/:id/unban', async (req, res) => {
+    try {
+        const userUnban = await prisma.user.update({
+            where: {
+                id: +req.params.id
+            },
+            data: {
+                banned: false
+            }
+        })
+        if (!userUnban) throw 'Empty'
+        res.status(200).send(userUnban)
+    } catch (err) {
+        if (err = 'Empty') res.status(404).send('This user does not exist')
+    }
+})
 
 export default router;
