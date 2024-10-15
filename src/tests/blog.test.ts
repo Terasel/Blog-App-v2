7a2c052321d2298a59994b7c3c15cee539dcc30f
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-10-15 13:51:17 +0200

    tests v1

--- src/tests/blog.test.ts
+++ src/tests/blog.test.ts
@@ -1,27 +1,75 @@
-// import { app } from '../domain/index'
-// import request from 'supertest'
-// import { createUser, updateUsername } from '../functionst-without-context'
-// import { prismaMock } from '../singleton'
-// describe('Blog API', () => {
+import { app } from '../domain/index'
+import request from 'supertest'
+describe('Blog API', () => {
 
-//     it('should create a new blog for a user', async () => {
-//         const userRes = await request(app)
-//             .post('/api/users')
-//             .send({
-//                 email: 'johndoe88@hotmail.com',
-//                 name: 'John Doe',
-//                 role: 'admin'
-//             });
-//         const blogRes = await request(app)
-//             .post(`/api/blog`)
-//             .send({
-//                 title: 'Somewhere over the rainbow',
-//                 authorId: 1
-//             });
-//         expect(blogRes.statusCode).toEqual(201);
-//         expect(blogRes.body).toHaveProperty('id');
-//         expect(blogRes.body.title).toBe('Somewhere over the rainbow');
-//         expect(blogRes.body.authorId).toBe(1);
-//     });
+    it('should create a new user', async () => {
+        const emailGen = 'whoknows' + Math.floor(Math.random() * 10).toString() + '@hotmail.com'
+        const userRes = await request(app)
+            .post('/api/users')
+            .send({
+                email: emailGen,
+                name: 'Mysterious Stranger',
+                role: 'admin'
+            });
+
+        expect(userRes.statusCode).toEqual(201);
+        expect(userRes.body.email).toBe(emailGen);
+        expect(userRes.body.name).toBe('Mysterious Stranger');
+        expect(userRes.body.role).toBe('admin');
+    });
+    it('should create a new blog', async () => {
+        const blogRes = await request(app)
+            .post(`/api/blog`)
+            .send({
+                title: 'Somewhere over the rainbow',
+                authorId: 1
+            });
+        expect(blogRes.statusCode).toEqual(201);
+        expect(blogRes.body.title).toBe('Somewhere over the rainbow');
+        expect(blogRes.body.authorId).toBe(1);
+    })
+})
+// import { MockContext, Context, createMockContext } from '../infrastructure/context'
+// import { createBlog, updateBlog } from '../controller/blog.controller'
+// import { createUser, updateUser } from '../controller/user.controller'
+
+// let mockCtx: MockContext
+// let ctx: Context
+
+// beforeEach(() => {
+//     mockCtx = createMockContext()
+//     ctx = mockCtx as unknown as Context
 // })
 
+// test('should create new user ', async () => {
+//     const user = {
+//         id: 1,
+//         email: 'hello@prisma.io',
+//         name: 'Rich',
+//         bio: null,
+//         role: 'admin',
+//         banned: false
+//     }
+//     mockCtx.prisma.user.create.mockResolvedValue(user)
+
+//     await expect(createUser(req: Request, res: Response)).resolves.toEqual({
+//         id: 1,
+//         email: 'hello@prisma.io',
+//         name: 'Rich',
+//         bio: null,
+//         role: 'admin',
+//         banned: false
+//     })
+// })
+// import { PrismaClient } from "@prisma/client";
+// import {prisma} from '../infrastructure/dbconnection'
+// const mockedPrismaClient = new (<new () => PrismaClient>(
+//     PrismaClient
+//   ))() as jest.Mocked<PrismaClient>;
+
+//   describe ('Blog tests', ()=> {
+//     afterEach(() => {
+//         jest.resetAllMocks();
+//       });
+
+//   }) 
