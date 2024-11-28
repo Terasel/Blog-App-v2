10c7797bcf2cbf3bfb051ca94f40cacbaada24d5
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-11-28 13:54:25 +0100

    user update v1

--- src/frontend/src/components/UserEdit.jsx
+++ src/frontend/src/components/UserEdit.jsx
@@ -1,26 +1,72 @@
 import { useEffect, useState } from "react"
 
-function UserEdit({ useredit }) {
-    // const [user, setUser] = useState([])
+function UserEdit() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [bio, setBio] = useState('')
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const options = { method: 'GET' }
-    //         const response = await fetch('http://localhost:3000/api/users/:id', options)
-    //         const data = await response.json()
-    //         setUser(data)
-    //     }
-    //     fetchData()
-    // }, [])
+    const submitData = async (e) => {
+        e.preventDefault()
+        // const userL = {
+        //     name, email, bio
+        // }
+        // const response = await fetch('http://localhost:3000/api/login', {
+        //     method: 'POST',
+        //     credentials: "include",
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify(userL)
+        // })
+        const user = {
+            name, email, bio
+        }
+        console.log(user)
+        const cookie = document.cookie
+        const update = await fetch(`http://localhost:3000/api/users/${cookie.id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', 'Cookie': cookie },
+            body: JSON.stringify(user)
+        })
+        console.log(update)
+        setName('')
+        setEmail('')
+        setBio('')
+    }
 
     return (
         <div>
-            <input type="text" name="name" placeholder="Name placeholder" />
-            <input type="text" name="email" placeholder="Email placeholder" />
-            <input type="text" name="bio" placeholder="Bio placeholder" />
-            <button type="button">Submit changes</button>
+            <form onSubmit={submitData}>
+                <label>User name:</label>
+                <input
+                    type="text"
+                    value={name}
+                    required
+                    onChange={(e) => setName(e.target.value)} />
+                <label>User email:</label>
+                <input
+                    type="text"
+                    value={email}
+                    required
+                    onChange={(e) => setEmail(e.target.value)} />
+                <label>User bio:</label>
+                <input
+                    type="text"
+                    value={bio}
+                    required
+                    onChange={(e) => setBio(e.target.value)} />
+                <button type="submit">Update</button>
+            </form>
         </div>
     )
+
+
+    // return (
+    //     <div>
+    //         <input type="text" name="name" placeholder="Name placeholder" />
+    //         <input type="text" name="email" placeholder="Email placeholder" />
+    //         <input type="text" name="bio" placeholder="Bio placeholder" />
+    //         <button type="button">Submit changes</button>
+    //     </div>
+    // )
 }
 
 export default UserEdit
